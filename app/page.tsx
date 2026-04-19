fd263167469e8d516db906cbb35b222ad686a9bd
YourBenies <broemen@yourbenies.com>
2026-04-19 08:44:26 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     }
     setLoading(true);
     try {
-      const res = await fetch('/api/subscribe/', {
+     await fetch('/api/subscribe', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
